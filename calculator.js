90dde36530681589d23b74ac353e998ddb321b0a
Henry <manojlostanarevic@gmail.com>
2025-05-27 11:41:28 +0200

    error handling

--- calculator.js
+++ calculator.js
@@ -1,13 +1,43 @@
+let flag = false;
+
+
+function rezultat(){
+  for (let i = 0 ; i < document.querySelector('.neprikaz').innerHTML.length; i++) {
+    if (i === 0 && (document.querySelector('.neprikaz').innerHTML[i] === "/" || document.querySelector('.neprikaz').innerHTML[i] === "*" || document.querySelector('.neprikaz').innerHTML[i] === "%")) {
+      document.querySelector('.prikaz').innerHTML = "ne moze to";
+      document.querySelector('.neprikaz').innerHTML = "";
+    } 
+
+  }
+  document.querySelector('.prikaz').innerHTML= eval(document.querySelector('.neprikaz').innerHTML);
+  document.querySelector('.neprikaz').innerHTML= eval(document.querySelector('.neprikaz').innerHTML);
+  flag = true;
+}
 
 
 function obrisiJedan() {
-  document.querySelector('.prikaz').innerHTML = document.querySelector('.prikaz').innerHTML.slice(0, -1) ;
+  if (flag) {
+    document.querySelector('.prikaz').innerHTML = "";
+    document.querySelector('.neprikaz').innerHTML = "";
+    flag = false;
+  }
+  else {
+    document.querySelector('.prikaz').innerHTML = document.querySelector('.prikaz').innerHTML.slice(0, -1);
+    document.querySelector('.neprikaz').innerHTML = document.querySelector('.neprikaz').innerHTML.slice(0, -1);
+  }
+  
 }
 
 function obrisiSkroz() {
   document.querySelector('.prikaz').innerHTML = "";
+  document.querySelector('.neprikaz').innerHTML = "";
+  flag = false;
 }
 
 function dodajZnak(znak) {
-  document.querySelector('.prikaz').innerHTML = znak;
+  document.querySelector('.prikaz').innerHTML += znak;
+  document.querySelector('.neprikaz').innerHTML += znak;
+  flag = false;
 }
+
+console.log(Number(eval("/23")));
