28e40660e9a56cf541a50faa6c2b1f09cbda8501
Henry <manojlostanarevic@gmail.com>
2025-05-27 14:25:58 +0200

    final version- one button does nothing, i got a lil lazy

--- calculator.js
+++ calculator.js
@@ -4,10 +4,11 @@ let flag = false;
 function rezultat(){
   for (let i = 0 ; i < document.querySelector('.neprikaz').innerHTML.length; i++) {
     if (i === 0 && (document.querySelector('.neprikaz').innerHTML[i] === "/" || document.querySelector('.neprikaz').innerHTML[i] === "*" || document.querySelector('.neprikaz').innerHTML[i] === "%")) {
-      document.querySelector('.prikaz').innerHTML = "ne moze to";
+      document.querySelector('.prikaz').innerHTML = "error";
       document.querySelector('.neprikaz').innerHTML = "";
+      return;
     } 
-
+    
   }
   document.querySelector('.prikaz').innerHTML= eval(document.querySelector('.neprikaz').innerHTML);
   document.querySelector('.neprikaz').innerHTML= eval(document.querySelector('.neprikaz').innerHTML);
@@ -16,15 +17,23 @@ function rezultat(){
 
 
 function obrisiJedan() {
-  if (flag) {
+  if (document.querySelector('.prikaz').innerHTML === "error"){
     document.querySelector('.prikaz').innerHTML = "";
     document.querySelector('.neprikaz').innerHTML = "";
     flag = false;
   }
   else {
-    document.querySelector('.prikaz').innerHTML = document.querySelector('.prikaz').innerHTML.slice(0, -1);
-    document.querySelector('.neprikaz').innerHTML = document.querySelector('.neprikaz').innerHTML.slice(0, -1);
+    if (flag) {
+      document.querySelector('.prikaz').innerHTML = "";
+      document.querySelector('.neprikaz').innerHTML = "";
+      flag = false;
+    }
+    else {
+      document.querySelector('.prikaz').innerHTML = document.querySelector('.prikaz').innerHTML.slice(0, -1);
+      document.querySelector('.neprikaz').innerHTML = document.querySelector('.neprikaz').innerHTML.slice(0, -1);
+    }
   }
+  
   
 }
 
@@ -35,9 +44,49 @@ function obrisiSkroz() {
 }
 
 function dodajZnak(znak) {
-  document.querySelector('.prikaz').innerHTML += znak;
-  document.querySelector('.neprikaz').innerHTML += znak;
   flag = false;
+  if (document.querySelector('.prikaz').innerHTML === "error"){
+    document.querySelector('.prikaz').innerHTML = "";
+    document.querySelector('.neprikaz').innerHTML = "";
+    document.querySelector('.prikaz').innerHTML += znak;
+    document.querySelector('.neprikaz').innerHTML += znak;
+    
+  }
+  else if (znak === "%" || znak === "*" || znak === "-" || znak === "+" || znak === "/" || znak === "."){
+    if (document.querySelector('.prikaz').innerHTML[document.querySelector('.prikaz').innerHTML.length - 1] === "%" || document.querySelector('.prikaz').innerHTML[document.querySelector('.prikaz').innerHTML.length - 1] === "*" || document.querySelector('.prikaz').innerHTML[document.querySelector('.prikaz').innerHTML.length - 1] === "/" || document.querySelector('.prikaz').innerHTML[document.querySelector('.prikaz').innerHTML.length - 1] === "+" || document.querySelector('.prikaz').innerHTML[document.querySelector('.prikaz').innerHTML.length - 1] === "-" || document.querySelector('.prikaz').innerHTML[document.querySelector('.prikaz').innerHTML.length - 1] === ".") {
+      
+      document.querySelector('.prikaz').innerHTML = document.querySelector('.prikaz').innerHTML.slice(0, -1);
+      document.querySelector('.neprikaz').innerHTML = document.querySelector('.neprikaz').innerHTML.slice(0, -1);
+      document.querySelector('.prikaz').innerHTML += znak;
+      document.querySelector('.neprikaz').innerHTML += znak;
+      
+      if (znak === "%") {
+        document.querySelector('.neprikaz').innerHTML = document.querySelector('.neprikaz').innerHTML.slice(0, -1);
+        document.querySelector('.neprikaz').innerHTML += "*0.01";
+        rezultat()
+        
+      }
+      
+    }
+    else {
+      if (znak === "%") {
+        document.querySelector('.prikaz').innerHTML += znak;
+        document.querySelector('.neprikaz').innerHTML += "*0.01";
+        rezultat()
+        
+      }
+      else {
+        document.querySelector('.prikaz').innerHTML += znak;
+        document.querySelector('.neprikaz').innerHTML += znak;
+        
+      }
+      
+    }
+  }
+  else { 
+    document.querySelector('.prikaz').innerHTML += znak;
+    document.querySelector('.neprikaz').innerHTML += znak;
+    
+  }
+  
 }
-
-console.log(Number(eval("/23")));
